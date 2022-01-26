91e7d4f7cb52b5a42da1f5dbb5d199e7bfe0f563
Rio Arjuna <ri0arjuna@programmer.net>
2022-01-26 22:43:32 +0700

    :tada: Initial commit.

--- app/src/main/java/dev/arj/banner/MainActivity.kt
+++ app/src/main/java/dev/arj/banner/MainActivity.kt
@@ -1,11 +1,25 @@
 package dev.arj.banner
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var containerBanner: ConstraintLayout
+  private lateinit var buttonConnectMic: MaterialButton
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    containerBanner = findViewById(R.id.container_banner)
+    buttonConnectMic = findViewById(R.id.button_connect_mic)
+
+    buttonConnectMic.setOnClickListener {
+      containerBanner.visibility = View.GONE
+    }
   }
 }
